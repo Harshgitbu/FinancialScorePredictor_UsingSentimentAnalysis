b521a438e2f062638495b660fe492d334fcd94d3
Harsh Shah <harshbu@bu.edu>
2025-04-24 18:32:04 -0400

    Update app.py

--- streamlitApp/app.py
+++ streamlitApp/app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from pages import sentiment_analysis
-from pages import fundamental_analysis
-from pages import technical_analysis
-from pages import final_verdict
-from pages import compare_stocks
+from modules import sentiment_analysis
+from modules import fundamental_analysis
+from modules import technical_analysis
+from modules import final_verdict
+from modules import compare_stocks
 
 st.set_page_config(page_title="Stock Verdict Intelligence", layout="wide")
 
