24d5aaa3caa4b4a82bd48958841bd1d0b3285876
Harsh Shah <harshbu@bu.edu>
2025-04-25 09:56:11 -0400

    fix path

--- streamlitApp/technical_analysis_page.py
+++ streamlitApp/technical_analysis_page.py
@@ -8,10 +8,10 @@ def run():
     st.title("📉 Technical Analysis")
 
     # Load technical indicator data
-    tech_df = pd.read_csv("C:/Users/ishan/Desktop/ISHANAY/BU docs/Spring 2025/Financial_analytics/Project/FinancialScorePredictor_UsingSentimentAnalysis/data/technical_indicators_wrds_output.csv")
+    tech_df = pd.read_csv("/data/technical_indicators_wrds_output.csv")
     tickers = tech_df["TICKER"].unique()
 
-    txt = open("C:/Users/ishan/Desktop/ISHANAY/BU docs/Spring 2025/Financial_analytics/Project/FinancialScorePredictor_UsingSentimentAnalysis/data/company_name_ticker.txt").read().strip()
+    txt = open("/data/company_name_ticker.txt").read().strip()
     # # Ticker selector
     # selected_ticker = st.selectbox("Select a Ticker:", tickers)
     mapping = ast.literal_eval("{" + txt + "}")
